60dd02e0a299fe1f0477a5405f5b21126a460e65
Farid Banihashemi <farid.banihasheni@gmail.com>
2023-01-16 23:09:06 +0100

    Removed Button class to Menu class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pygame
 import sys
 from constats import *
+from menu import Menu
 import random
 import string
 from game import Game
@@ -12,43 +13,30 @@ pygame.init()
 screen = pygame.display.set_mode(w_size)
 pygame.display.set_caption('Memory Game')
 screen.fill(BG_COLOR)
-m_screen = pygame.Surface((WIDTH, MENU_HITHT))
-m_screen.fill(BG_COLOR)
 
 font = pygame.font.SysFont("Arial", 20)
-
-class Button:
-    def __init__(self, text) -> None:
-        self.text = font.render(text, 1, LINE_COLOR, BG_COLOR)
-        self.size = self.text.get_size()
-        self.surface = pygame.Surface(self.size)
-    
-    def show(self):
-        self.surface.blit(self.text, (0, 0))
-        m_screen.blit(self.surface, (10, 10))
-        
+     
 # The man function to start the game
 def main():
     clock = pygame.time.Clock()
     game = Game(screen)
     game.draw_grid()
-    reset_button = Button("Reset")
     game.laod_images()
     old_item = None
     start = datetime.now()
     while True:
-        screen.blit(m_screen, (0, 0))
-        reset_button.show()
+        menu = Menu()
+        screen.blit(menu.surface, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
+                pos = event.pos #pygame.mouse.get_pos()
                 if pos[1] > MENU_HITHT:
                     raw, col = (pos[1] - MENU_HITHT) // SQR_SIZE[1], pos[0] // SQR_SIZE[0]
                     choosed_item = (raw, col)
-                    if game.mode == 'alf':
+                    if game.mode == 'alfa':
                         choosen_value = (game.grid[raw, col])
                     elif game.mode == 'img':
                         choosen_value = (game.grid[raw, col]).id
@@ -70,7 +58,17 @@ def main():
                             game.heid_items(old_item, choosed_item)
                         old_item = None
                 else:
-                    if 10 < pos[0] < reset_button.size[0]:
+                    print(pos)
+                    print('reset :', menu.reset.rect)
+                    print('alfa :', menu.alfa.rect)
+                    print('img :', menu.img.rect)
+                    if menu.reset.rect.collidepoint(pos): # menu.reset.rect.left < pos[0] < menu.reset.rect.right:
+                        game.reset()
+                    if menu.alfa.rect.collidepoint(pos): # menu.reset.rect.left < pos[0] < menu.alfa.rect.right:
+                        game.mode = 'alfa'
+                        game.reset()
+                    if menu.img.rect.collidepoint(pos): # menu.img.rect.left < pos[0] < menu.img.rect.right:
+                        game.mode = 'img'
                         game.reset()
 
                 
